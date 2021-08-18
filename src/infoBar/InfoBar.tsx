4527c77afdbe0f488ecfdaf2dc07c4508f3ee614
Fabian <edv@sozarch.uzh.ch>
2021-08-18 15:24:40 +0200

    keyDown Buttons activated, refs #7

--- src/infoBar/InfoBar.tsx
+++ src/infoBar/InfoBar.tsx
@@ -1,36 +1,42 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
 import "./infoBar.css";
 import Tabs from "./Tabs";
+import {AppContext} from "../AppContext";
 
 
 export default function InfoBar() {
 
-    /*const keyDown = function(event: KeyboardEvent) {
-        if (event.key === 'ArrowLeft') {
-            previousPage();
-            return;
-        }
-
-        if (event.key === 'ArrowRight') {
-            nextPage();
-            return;
-        }
-
-        if (props.presentation.searchService && (event.ctrlKey|| event.metaKey) && event.key === 'f') {
-            event.preventDefault();
-            props.setTab('search');
-            return;
-        }
-    }*/
-
-
-
-    /*useEffect(() => {
+    const {page, setPage, currentManifest, setTab} = useContext(AppContext);
+    useEffect(() => {
         document.addEventListener("keydown", keyDown, false);
         return () => {
             document.removeEventListener('keydown', keyDown, false);
         };
-    });*/
+    });
+
+    if (!currentManifest) {
+        return <></>;
+    }
+
+    const keyDown = function(event: KeyboardEvent) {
+
+        if (event.key === 'ArrowLeft' && page > 0) {
+            setPage(page - 1);
+            return;
+        }
+
+        if (event.key === 'ArrowRight' && (page + 1) < currentManifest?.images.length) {
+            setPage(page + 1);
+            return;
+        }
+
+        if (currentManifest.search && (event.ctrlKey|| event.metaKey) && event.key === 'f') {
+            event.preventDefault();
+            setTab('search');
+            return;
+        }
+    }
+
 
     return <div className="aiiif-infobar">
         <Tabs />
