94ce7cd59f4cad5c1a7e3e8c63fc2f106ece3961
Fabian <edv@sozialarchiv.ch>
2025-10-17 01:57:58 +0200

    ie11 support removed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import 'core-js';
-import 'react-app-polyfill/ie11';
 import Init from './Init';
 import ManifestData from './entity/ManifestData';
 import ManifestDataThumbnail from './entity/ManifestDataThumbnail';
